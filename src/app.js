5b69fae87f8b27866f6d78da6f86d33ca512867d
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2023-10-26 00:02:45 +0000

    pronto etapa 1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let numeros = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "A",
+  "J",
+  "Q",
+  "K"
+];
+let simbolos = ["❤", "♠", "♦", "♣"];
+
+function randomCard() {
+  let randomSimbolos = simbolos[Math.floor(Math.random() * simbolos.length)];
+  let randomNumeros = numeros[Math.floor(Math.random() * numeros.length)];
+
+  let symbolUp = document.querySelector(".symbol-up");
+  let symbolDown = document.querySelector(".symbol-down");
+  let number = document.querySelector(".number");
+
+  symbolUp.textContent = randomSimbolos;
+  symbolDown.textContent = randomSimbolos;
+  number.textContent = randomNumeros;
+}
+
+window.onload = randomCard();
